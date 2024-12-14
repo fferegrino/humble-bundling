64abb5436c48d6c5d7407b61c26c7fa6092ca150
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-14 20:16:42 +0000

    Add nested dict popping

--- functions.py
+++ functions.py
@@ -13,6 +13,12 @@ def prepare_to_remove(to_remove):
 def _remove_keys(data, single_key_to_remove):
     if isinstance(single_key_to_remove, str):
         data.pop(single_key_to_remove, None)
+    elif isinstance(single_key_to_remove, list):
+        first_key = single_key_to_remove[0]
+        if len(single_key_to_remove) == 1:
+            data.pop(first_key, None)
+        elif isinstance(data[first_key], dict):
+            _remove_keys(data[first_key], single_key_to_remove[1:])
 
 
 def remove_keys(data, keys_to_remove):
@@ -37,19 +43,19 @@ def test_simple_one_level():
     assert expected == input
 
 
-# def test_simple_two_level():
-#     input = {
-#         "a": {
-#             "b": 2,
-#             "c": 3,
-#         }
-#     }
-#     expected = {
-#         "a": {
-#             "b": 2,
-#         }
-#     }
-#     actual = remove_keys(input, [["a", "c"]])
+def test_simple_two_level():
+    input = {
+        "a": {
+            "b": 2,
+            "c": 3,
+        }
+    }
+    expected = {
+        "a": {
+            "b": 2,
+        }
+    }
+    actual = remove_keys(input, [["a", "c"]])
 
-#     assert actual == expected
-#     assert expected == input
+    assert actual == expected
+    assert expected == input
