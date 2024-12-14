31c987bade1e077a8069692031efbc2a14d97ccb
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-14 20:29:20 +0000

    work with dicts

--- functions.py
+++ functions.py
@@ -18,7 +18,9 @@ def _remove_keys(data, single_key_to_remove):
         if first_key == "[]" and isinstance(data, list):
             for item in data:
                 _remove_keys(item, single_key_to_remove[1:])
-
+        elif first_key == "*" and isinstance(data, dict):
+            for key, value in data.items():
+                _remove_keys(value, single_key_to_remove[1:])
         elif len(single_key_to_remove) == 1:
             data.pop(first_key, None)
         elif isinstance(data[first_key], dict):
@@ -31,6 +33,38 @@ def remove_keys(data, keys_to_remove):
     for key in keys_to_remove:
         _remove_keys(data, key)
     return data
+
+
+def test_arbitrary_dict_keys():
+    input = {
+        "a": {
+            "b1": {
+                "hello": "world",
+                "c": 1,
+            },
+            "b2": {
+                "hello": "world",
+                "c": 2,
+            },
+            "b3": {
+                "hello": "world",
+                "d": 2,
+            },
+        }
+    }
+
+    expected = {
+        "a": {
+            "b1": {"hello": "world"},
+            "b2": {"hello": "world"},
+            "b3": {"hello": "world", "d": 2},
+        }
+    }
+
+    actual = remove_keys(input, [["a", "*", "c"]])
+
+    assert actual == expected
+    assert expected == input
 
 
 def test_with_list_level():
