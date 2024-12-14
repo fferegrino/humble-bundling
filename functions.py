f3d15f4f65664e70e12625cb096af928fd20d683
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-14 20:11:59 +0000

    Base work generalizable

--- functions.py
+++ functions.py
@@ -4,18 +4,20 @@ to_remove = {
     "y.z",
 }
 
+
 def prepare_to_remove(to_remove):
-    to_remove_list = [
-        key.split(".")
-        for key in to_remove
-    ]
+    to_remove_list = [key.split(".") for key in to_remove]
     return sorted(to_remove_list, key=lambda x: len(x), reverse=True)
 
+
+def _remove_keys(data, single_key_to_remove):
+    if isinstance(single_key_to_remove, str):
+        data.pop(single_key_to_remove, None)
 
 
 def remove_keys(data, keys_to_remove):
     for key in keys_to_remove:
-        data.pop(key, None)
+        _remove_keys(data, key)
     return data
 
 
@@ -33,3 +35,21 @@ def test_simple_one_level():
 
     assert actual == expected
     assert expected == input
+
+
+# def test_simple_two_level():
+#     input = {
+#         "a": {
+#             "b": 2,
+#             "c": 3,
+#         }
+#     }
+#     expected = {
+#         "a": {
+#             "b": 2,
+#         }
+#     }
+#     actual = remove_keys(input, [["a", "c"]])
+
+#     assert actual == expected
+#     assert expected == input
