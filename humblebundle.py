8beb0ae8b911525ef8acc8b561638d72e188988d
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-14 11:57:55 +0000

    Change reader

--- humblebundle.py
+++ humblebundle.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import json
 from datetime import datetime
 from collections import namedtuple
-import csv
 from pathlib import Path
 
 data_dir = Path("data")
@@ -14,14 +13,16 @@ Entry = namedtuple('Entry', ['query_time','kind', 'url','content'])
 query_time = datetime.now()
 query_time_str = query_time.isoformat()
 
-def load_js_data(page, script_id):
+def load_js_data(page, script_id, extract_key=None):
     script_id = script_id.lstrip("/")
     page = requests.get(f"https://www.humblebundle.com/{page}")
     soup = BeautifulSoup(page.content, "html.parser")
     data = json.loads(soup.find('script', {'id':script_id}).text)
+    if extract_key:
+        data = data[extract_key]
     return data
 
-all_bundles_data = load_js_data("bundles", "landingPage-json-data")
+all_bundles_data = load_js_data("bundles", "landingPage-json-data", 'data')
 
 all_bundles_data['data'].keys()
 
@@ -31,14 +32,38 @@ entries = [
 
 for kind in all_bundles_data['data'].keys():
     for product in all_bundles_data['data'][kind]['mosaic'][0]['products']:
-        product_data = load_js_data(product['product_url'], 'webpack-bundle-page-data')
+        product_data = load_js_data(product['product_url'], 'webpack-bundle-page-data', 'bundleData')
         entries.append(Entry(query_time_str, kind, product['product_url'], json.dumps(product_data)))
 
 monthly_file = query_time.strftime("%Y-%m")
-monthly_file
 
-with open(data_dir / f"{monthly_file}.csv", "a") as f:
-    writer = csv.writer(f)
+def remove_empty_keys(data):
+    empty_keys = []
+    for key, value in data.items():
+        if value is None:
+            empty_keys.append(key)
+        elif isinstance(value, dict):
+            remove_empty_keys(value)
+            if not value:
+                empty_keys.append(key)
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    remove_empty_keys(item)
+                    if not item:
+                        empty_keys.append(key)
+                elif isinstance(item, str):
+                    if not item:
+                        empty_keys.append(key)
+    for key in empty_keys:
+        del data[key]
+    return data
+
+with open(data_dir / f"{monthly_file}.jsonl", "a") as f:
     for entry in entries:
-        writer.writerow(entry)
+        data_thing = remove_empty_keys(entry.content)
+        data_thing['_ts'] = entry.query_time
+        data_thing['_kind'] = entry.kind
+        data_thing['_url'] = entry.url
+        f.write(json.dumps(data_thing) + "\n")
 
